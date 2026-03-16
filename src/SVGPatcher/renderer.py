ac60ca717fdda79e31578b9c78eae74395f8bde0
FayadMayo <alfayad.abdulrahman@mayo.edu>
2026-03-16 13:56:45 -0500

    feat: make svg rendering rely on python binding to resvg

--- src/SVGPatcher/renderer.py
+++ src/SVGPatcher/renderer.py
@@ -1,28 +1,9 @@
-import os
-import subprocess
 from lxml import etree as ET
+import resvg_py
 
 
-def render_png(
-    svg, outpath: str, zoom: int = 2, dpi: int | float = 300, keep_out_svg: bool = False
-):
-    out_svg = outpath.replace(".png", ".svg")
-    et = ET.ElementTree(svg)
-    et.write(out_svg, pretty_print=True)
-    subprocess.run(
-        [
-            "resvg",
-            "-z",
-            str(zoom),
-            "--dpi",
-            str(dpi),
-            "--image-rendering",
-            "high-quality",
-            out_svg,
-            outpath,
-        ],
-        capture_output=True,
-        text=True,
-    )
-    if not keep_out_svg:
-        os.remove(out_svg)
+def render_png(svg, outpath: str, zoom: int = 2, dpi: int | float = 300):
+    svg_string = ET.tostring(svg, encoding="utf-8").decode("utf-8")
+    png_output = resvg_py.svg_to_bytes(svg_string=svg_string, zoom=zoom, dpi=dpi)
+    with open(outpath, "wb") as binary_file:
+        binary_file.write(png_output)
